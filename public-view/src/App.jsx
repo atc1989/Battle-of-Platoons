47ccf2ef5353137592ef547fb0f13abb2c28ac15
najeebiee <nmapantas_240000002090@uic.edu.ph>
2026-01-07 18:39:53 +0800

    yes

--- public-view/src/App.jsx
+++ public-view/src/App.jsx
@@ -591,7 +591,6 @@ function Podium({ top3, view }) {
           >
             <motion.div
               className={cardClass}
-              data-card-class={cardClass}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.4, delay: index * 0.1 }}
